7a96960f7f3c5b8110826c66a03607fd4f5aeabf
Ghosh <kaustavg@3c22fb229ff6.ant.amazon.com>
2021-11-09 16:34:50 -0800

    add amplify backend files

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,35 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "consoleexttestf04f3d82": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "function": {
+        "S3Trigger8dbd49b1": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
+    },
+    "storage": {
+        "s3b45affc6": {
+            "BucketName": "string",
+            "Region": "string"
+        },
+        "dynamo9749af4b": {
+            "Name": "string",
+            "Arn": "string",
+            "StreamArn": "string",
+            "PartitionKeyName": "string",
+            "PartitionKeyType": "string",
+            "Region": "string"
+        }
+    }
+}
